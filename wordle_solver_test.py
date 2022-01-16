91633faf6496dafe272a5af7367e796691dd0340
Julio Ernesto Rodríguez Cabañas <julioernesto90@gmail.com>
2022-01-16 12:12:25 +0000

    Improve test output

--- wordle_solver_test.py
+++ wordle_solver_test.py
@@ -9,15 +9,15 @@ def test_solutions(solutions):
     solutions_found = []
     solutions_not_found = []
 
-    for solution_to_test in solutions:
+    for index, solution_to_test in enumerate(solutions):
         expected_solution = solution_to_test
         solution, guesses = wordle_solver.play_wordle(get_result_input = get_result_input)
         if solution != None:
             solutions_found.append((solution, guesses))
-            print('🟩 Solution found for ' + expected_solution.upper() + ':\n> ' + str(guesses))
+            print('🟩 Test ' + str(index + 1) + ' / ' + str(len(solutions)) + ': "' + expected_solution.upper() + '" => ' + str(guesses))
         else:
             solutions_not_found.append((solution, guesses))
-            print('🟥 No solution found for ' + expected_solution.upper() + ':\n> ' + str(guesses))
+            print('🟥 Test ' + str(index + 1) + ' / ' + str(len(solutions)) + ': "' + expected_solution.upper() + '" => ' + str(guesses))
 
     return solutions_found, solutions_not_found
 
@@ -37,4 +37,7 @@ def get_result_input(guess, _):
     return result_input
 
 solutions_found, solutions_not_found = test_solutions(wordle_data.solutions)
-print('\nValid solutions found for ' + str(len(solutions_found)) + ' out of ' + str(len(wordle_data.solutions)) + ' (' + str(round((len(solutions_found) * 100.0) / len(wordle_data.solutions), 2)) + '%).')
+print(
+    '\nValid solutions found for ' + str(len(solutions_found)) + ' out of ' + str(len(wordle_data.solutions)) + ' cases' + \
+    ' (' + str(round((len(solutions_found) * 100.0) / len(wordle_data.solutions), 2)) + '%)' + \
+    ' with an average of ' + str(round(sum(len(guesses) for _, guesses in solutions_found) / len(solutions_found), 2)) + ' guesses per found solution.')
